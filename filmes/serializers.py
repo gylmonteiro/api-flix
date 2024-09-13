40de51ba1b94070a23490d2c26ee935c9b7b1710
Gyl Monteiro <gyldeveloper@gmail.com>
2024-09-13 12:23:56 -0300

    Modificado o serializador, de model para um manual. Feito um teste

--- filmes/serializers.py
+++ filmes/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from .models import Filme
+from generos.models import Genero
+from atores.models import Ator
+
+#Criamos este exemplo de um serializer manual. Ofertando maior poder de personalização
+class FilmeSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    titulo = serializers.CharField()
+    genero = serializers.PrimaryKeyRelatedField(queryset = Genero.objects.all())
+    ano_de_lancamento = serializers.IntegerField()
+    atores = serializers.PrimaryKeyRelatedField(queryset = Ator.objects.all(), many=True)
+    resumo = serializers.CharField()
+    
+    def create(self, validated_data):
+        todos_atores = validated_data.pop('atores')
+    
+        filme = Filme.objects.create(**validated_data)
+        filme.atores.set(todos_atores)
+        return filme
 
 
-class FilmeSerializer(serializers.ModelSerializer):
+
+class FilmeModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Filme
         fields = '__all__'
+
+    def validate_ano_de_lancamento(self, value):
+
+        if value < 2000:
+            raise serializers.ValidationError("Não pode cadastrar menos de 2000")
+        return value
--- filmes/views.py
+++ filmes/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from .models import Filme
-from .serializers import FilmeSerializer
+from .serializers import FilmeModelSerializer
 
 # Create your views here.
 
 
 class FilmeListaCriaView(generics.ListCreateAPIView):
     queryset = Filme.objects.all()
-    serializer_class = FilmeSerializer
+    serializer_class = FilmeModelSerializer
 
 
 class FilmeDetalhaAtualizaDeletaView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Filme.objects.all()
-    serializer_class = FilmeSerializer
+    serializer_class = FilmeModelSerializer
