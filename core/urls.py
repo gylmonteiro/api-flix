a1042e431b7a12854ff7aaa43598b88660a4bafc
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-14 07:24:47 -0300

    resolvido conflito nas urls do projeto

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('api/v1/', include('generos.urls')),
     path('api/v1/', include('atores.urls')),
