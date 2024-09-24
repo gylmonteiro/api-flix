0c4a2730bf7c944be6bc85233cd4d148b36a03ab
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-09-24 07:33:35 -0300

    Protegido a url de avaliações com as permissões de classes

--- avaliacoes/views.py
+++ avaliacoes/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Avaliacao
 from .serializers import AvaliacaoSerializer
 
